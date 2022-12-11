35e55febc2c8dbb61d7d22795a7b3ad994a34727
Marek Souček <m1_soucek@utb.cz>
2022-12-11 12:30:11 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LyricApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
